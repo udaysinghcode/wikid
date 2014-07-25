e4c11c046720a23d6831d96dc47e4a67ebff491e
Uday Singh <udaysingh.code@gmail.com>
2014-07-25 00:12:51 -0400

    added Article model

---
+++ app/models/article.rb
@@ -0,0 +1,16 @@
+class Article < ActiveRecord::Base
+	belongs_to :user
+	has_many :categories through :article_categories
+
+	validates :title, presence: true
+	validates :content, presence: true
+	validates :categories, presence: true
+end
+
+=begin
+create_table :articles do |t|
+      t.string :title
+      t.text :content
+
+      t.timestamps
+=end
