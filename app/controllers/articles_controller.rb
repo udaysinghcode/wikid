cbcb18bb751ec8d8c2e69f3a6bb8cd5e9efbdf16
Uday Singh <udaysingh.code@gmail.com>
2014-09-30 17:05:21 -0400

    Added form partial and updated index, new, and articles controller to allow users to write articles.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,9 +8,21 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'articles/new'
+    end
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :content, :category_ids => [])
+    end
 
 end
