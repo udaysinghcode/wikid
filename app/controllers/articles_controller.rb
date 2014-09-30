2c2af34f1f8b923859b5b64afddb31da2cc2998f
Uday Singh <udaysingh.code@gmail.com>
2014-09-30 03:30:40 -0400

    articles controller added

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,16 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.order(updated_at: :desc).limit(25)
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+end
