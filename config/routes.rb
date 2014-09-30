7d54ccbd0aca137ce3aad532e2d55e568a279833
Uday Singh <udaysingh.code@gmail.com>
2014-09-30 19:45:36 -0400

    Allowed users to view categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+
+  get 'categories/show'
+
+  get 'categories_controller/index'
+
+  get 'categories_controller/show'
+
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -8,6 +16,7 @@ Rails.application.routes.draw do
   
   root 'welcome#index'
   resources :articles
+  resources :categories
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
